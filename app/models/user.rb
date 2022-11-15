a7fd2678db8ca8b60fab4e776c5ed9a73b081ed7
Heber Lazo <heberlazo@gmail.com>
2022-11-14 19:47:35 -0500

    Added more validations to user

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,7 @@
 class User < ApplicationRecord
-  validates_presence_of :name, :surname, :grade, :section, :email
+  validates :email, uniqueness: true
+  validates_format_of :email, with: /@/
+  validates_presence_of :name, :surname, :grade, :section, :email, :password_digest
   before_save :capitalize_all
 
   def capitalize_all
