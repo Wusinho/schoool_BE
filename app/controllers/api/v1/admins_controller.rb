8b4533eb976d24fe2536483179741171cb7bdaf0
Heber Lazo <heberlazo@gmail.com>
2022-11-15 10:51:18 -0500

    Added admin model

---
+++ app/controllers/api/v1/admins_controller.rb
@@ -0,0 +1,29 @@
+module Api
+  module V1
+    class AdminController < ApplicationController
+      def create
+        @admin = Admin.find_by_email(admin_params[:email])
+        if @admin&.authenticate(admin_params[:password])
+          render json: {
+            token: JsonWebToken.encode(admin_id: @admin.id),
+            name: @admin.name,
+            status: :created
+          }
+        else
+          render json: {
+            error: 'Wrong email/password',
+            status: :not_found
+          }
+        end
+      end
+
+      private
+      def admin_params
+        params.require(:admin).permit(:email, :password)
+      end
+
+    end
+
+  end
+end
+
