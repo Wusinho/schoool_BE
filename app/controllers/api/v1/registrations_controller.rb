ef5687f703c14287c35380f555b7dc617217c6c7
Heber Lazo <heberlazo@gmail.com>
2022-11-14 22:36:53 -0500

    Improved controller and routes

--- app/controllers/api/v1/registrations_controller.rb
+++ app/controllers/api/v1/registrations_controller.rb
@@ -1,21 +1,25 @@
-class Api::V1::RegistrationsController < ApplicationController
-  def create
-    @user = User.new(user_params)
+module Api
+  module V1
+    class RegistrationsController < ApplicationController
+      def create
+        @user = User.new(user_params)
 
-    if @user
-      render json: @user, status: :created
-    else
-      render json: @user.errors, status: :unprocessable_entity
+        if @user
+          render json: @user, status: :created
+        else
+          render json: @user.errors, status: :unprocessable_entity
+        end
+
+      end
+
+      def show
+        user = User.find(params[:id])
+      end
+      private
+
+      def user_params
+        params.require(:user).permit( :name, :surname, :grade, :section, :email, :password)
+      end
     end
-
-  end
-
-  def show
-    user = User.find(params[:id])
-  end
-  private
-
-  def user_params
-    params.require(:user).permit(:email, :password)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
-  namespace :api, defaults: { format: :json } do
+  namespace :api do
     namespace :v1 do
+      resources :registrations, only: [:create]
 
     end
   end
