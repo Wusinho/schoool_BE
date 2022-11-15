72c252884f1f6839100551ef1f4a62fc5296778a
Heber Lazo <heberlazo@gmail.com>
2022-11-15 13:06:28 -0500

    refactor migration and seed

--- db/migrate/20221115173321_create_edlevels.rb
+++ db/migrate/20221115173321_create_edlevels.rb
@@ -1,7 +1,8 @@
 class CreateEdlevels < ActiveRecord::Migration[7.0]
   def change
     create_table :edlevels do |t|
-      t.integer :ed_level
+      t.integer :ed_level, null: false
+      t.string :nivel, null: false
 
       t.timestamps
     end
