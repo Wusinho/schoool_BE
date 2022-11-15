fc29b3ca0ffda14134da9c8b715ac6abc869e5d1
Heber Lazo <heberlazo@gmail.com>
2022-11-15 10:57:37 -0500

    Added validations

--- app/models/admin.rb
+++ app/models/admin.rb
@@ -1,2 +1,14 @@
 class Admin < ApplicationRecord
+  has_secure_password
+  validates :email, uniqueness: true
+  validates_format_of :email, with: /@/
+  validates_presence_of :name, :surname, :email, :password_digest
+  before_save :capitalize_all
+
+  def capitalize_all
+    self.name = name.capitalize
+    self.surname = surname.capitalize
+    self.section = section.capitalize
+  end
+
 end
