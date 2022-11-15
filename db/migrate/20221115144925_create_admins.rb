ce2ea2e893bc9e84905cbe2755a64207531a5a2d
Heber Lazo <heberlazo@gmail.com>
2022-11-15 11:18:55 -0500

    Added migration and seed

---
+++ db/migrate/20221115144925_create_admins.rb
@@ -0,0 +1,16 @@
+class CreateAdmins < ActiveRecord::Migration[7.0]
+  def change
+    create_table :admins do |t|
+      t.string :name, null: false
+      t.string :surname, null: false
+      t.integer :level, null: false
+      t.integer :grade, default: ''
+      t.string :section, default: ''
+      t.string :email, null: false
+      t.index :email, unique:true
+      t.string :password_digest, null: false
+
+      t.timestamps
+    end
+  end
+end
---
+++ db/seeds.rb
@@ -0,0 +1,7 @@
+password = '123456'
+Admin.create(name: 'Admin', surname: 'San Miguel', level: 1, email: 'admin@gmail.com', password: password, password_confirmation: password)
+Admin.create(name: 'Profesor 1', surname: 'lopez', level: 2, grade: 5, section: 'A', email: 'profesor1@gmail.com', password: password, password_confirmation: password)
+Admin.create(name: 'Profesor 2', surname: 'lazo', level: 2, grade: 5, section: 'B', email: 'profesor2@gmail.com', password: password, password_confirmation: password)
+Admin.create(name: 'Profesor 3', surname: 'moreno', level: 2, grade: 5, section: 'C', email: 'profesor3@gmail.com', password: password, password_confirmation: password)
+Admin.create(name: 'Profesor 4', surname: 'santi', level: 2, grade: 5, section: 'D', email: 'profesor4@gmail.com', password: password, password_confirmation: password)
+Admin.create(name: 'Profesor 5', surname: 'salas', level: 2, grade: 5, section: 'E', email: 'profesor5@gmail.com', password: password, password_confirmation: password)
