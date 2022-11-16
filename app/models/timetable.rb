0517ff9750fe9eb9ac0cf70d9826ca07056f7b98
Heber Lazo <heberlazo@gmail.com>
2022-11-15 22:47:13 -0500

    Added timetable model

---
+++ app/models/timetable.rb
@@ -0,0 +1,4 @@
+class Timetable < ApplicationRecord
+  enum :week_day, [ :Lunes, :Martes, :Miercoles, :Jueves, :Viernes, :Sabado, :Domingo ]
+  validates_presence_of :starts_at, :ends_at
+end
---
+++ db/migrate/20221115164316_create_timetables.rb
@@ -0,0 +1,11 @@
+class CreateTimetables < ActiveRecord::Migration[7.0]
+  def change
+    create_table :timetables do |t|
+      t.integer :week_day, default: 0
+      t.time :starts_at, null: false
+      t.time :ends_at, null: false
+
+      t.timestamps
+    end
+  end
+end
