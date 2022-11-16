519325b7bb7b8edbadf3d14a8d995cae2c8ef61c
Heber Lazo <heberlazo@gmail.com>
2022-11-16 12:01:35 -0500

    refactor test routes and refactor student tests

--- spec/requests/api/v1/student/registrations_spec.rb
+++ spec/requests/api/v1/student/registrations_spec.rb
@@ -1,17 +1,17 @@
 require 'rails_helper'
 require 'helpers/authorization_helper'
 
-RSpec.describe "Api::v1::Registrations", type: :request do
+RSpec.describe "Api::v1::Student::Registrations", type: :request do
   include AuthorizationHelper
 
-  describe "POST /api/v1/registrations" do
+  describe "POST /api/v1/student/registrations" do
     let(:classroom) { create(:classroom) }
 
       describe 'with all the correct params' do
         it 'should create a post' do
           req_payload = correct_student_params
 
-          post "/api/v1/registrations", params: req_payload
+          post "/api/v1/student/registrations", params: req_payload
           payload = JSON.parse(response.body)
 
           expect(payload['name']).to_not be_empty
@@ -19,18 +19,18 @@ RSpec.describe "Api::v1::Registrations", type: :request do
         end
       end
 
-      describe 'with a missing strong param' do
-        it 'should not create a student when a required params is not sent' do
-          req_payload = missing_student_surname
+    describe 'with a missing strong param' do
+      it 'should not create a student when a required params is not sent' do
+        req_payload = missing_student_surname
 
-          post "/api/v1/registrations", params: req_payload
-          payload = JSON.parse(response.body)
-          resp = JSON.parse(response.body)
+        post "/api/v1/student/registrations", params: req_payload
+        payload = JSON.parse(response.body)
+        resp = JSON.parse(response.body)
 
-          expect(payload['error']).to eql({"surname"=>["can't be blank"]})
-          expect(resp['status']).to eql('unprocessable_entity')
-        end
+        expect(payload['error']).to eql({"surname"=>["can't be blank"]})
+        expect(resp['status']).to eql('unprocessable_entity')
       end
+    end
 
       describe 'with a duplicated email in DB' do
         let!(:student) { create(:student)}
@@ -38,7 +38,7 @@ RSpec.describe "Api::v1::Registrations", type: :request do
         it 'should not create student' do
           req_payload = duplicated_student_email
 
-          post "/api/v1/registrations", params: req_payload
+          post "/api/v1/student/registrations", params: req_payload
           payload = JSON.parse(response.body)
           resp = JSON.parse(response.body)
 
