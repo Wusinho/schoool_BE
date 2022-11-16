a0fdedea0d8e0e836d0769f4d79feaaab4fb961a
Heber Lazo <heberlazo@gmail.com>
2022-11-16 10:47:49 -0500

    Added registrations helper

---
+++ spec/helpers/authorization_helper.rb
@@ -0,0 +1,43 @@
+require 'faker'
+
+# authorization helper
+module AuthorizationHelper
+  def correct_student_params
+    {
+      registration:{
+        name: Faker::Name.first_name,
+        surname: Faker::Name.last_name,
+        email: Faker::Internet.email,
+        password: '123456',
+        password_confirmation: '123456',
+        classroom_id: classroom.id
+      }
+    }
+  end
+
+  def missing_student_surname
+    {
+      name: Faker::Name.first_name,
+      surname: '',
+      email: Faker::Internet.email,
+      password: '123456',
+      password_confirmation: '123456',
+      classroom_id: classroom.id
+    }
+  end
+
+  def register_user(user)
+    post '/api/v1/registrations', params: user
+  end
+
+  def users_session(user)
+    session_params = {
+      user: {
+        email: user[:user][:email],
+        password: user[:user][:password]
+      }
+    }
+    post '/api/v1/sessions', params: session_params
+    JSON.parse(response.body)
+  end
+end
--- spec/requests/registrations_spec.rb
+++ spec/requests/registrations_spec.rb
@@ -1,38 +1,24 @@
 require 'rails_helper'
-require 'faker'
+require 'helpers/authorization_helper'
 
 RSpec.describe "Api::v1::Registrations", type: :request do
+  include AuthorizationHelper
+
   describe "POST /api/v1/registrations" do
     let(:classroom) { create(:classroom) }
 
     it 'should create a post' do
-      req_payload ={
-        registration:{
-          name: Faker::Name.first_name,
-          surname: Faker::Name.last_name,
-          email: Faker::Internet.email,
-          password: '123456',
-          password_confirmation: '123456',
-          classroom_id: classroom.id
-        }
-      }
+      req_payload = correct_student_params
+
       post "/api/v1/registrations", params: req_payload
       payload = JSON.parse(response.body)
-      expect(payload).to_not be_empty
+      expect(payload['name']).to_not be_empty
       expect(response).to have_http_status(:ok)
     end
 
     it 'should not create a post when a required data is not sent' do
-      req_payload ={
-        registration:{
-          name: Faker::Name.first_name,
-          surname: '',
-          email: Faker::Internet.email,
-          password: '123456',
-          password_confirmation: '123456',
-          classroom_id: classroom.id
-        }
-      }
+      req_payload = missing_student_surname
+
       post "/api/v1/registrations", params: req_payload
       payload = JSON.parse(response.body)
       expect(payload).to_not be_empty
