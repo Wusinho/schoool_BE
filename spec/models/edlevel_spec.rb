5650c5e295874b81940aa879763f964347e8b867
Heber Lazo <heberlazo@gmail.com>
2022-11-16 08:10:35 -0500

    Corrected test for Edlevel

--- spec/models/edlevel_spec.rb
+++ spec/models/edlevel_spec.rb
@@ -5,7 +5,7 @@ RSpec.describe Edlevel, type: :model do
     context 'validations' do
       it do
         should validate_presence_of(:nivel)
-        should validate_presence_of(:edlevel)
+        should validate_presence_of(:ed_level)
       end
     end
   end
