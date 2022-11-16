69dba7e81cdf63d7df29d57512fade1e3e6e665c
Heber Lazo <heberlazo@gmail.com>
2022-11-16 12:51:31 -0500

    Added test for teacher session

--- spec/factories/teachers.rb
+++ spec/factories/teachers.rb
@@ -1,5 +1,11 @@
+require 'faker'
+
 FactoryBot.define do
   factory :teacher do
-    
+    name { Faker::Name.first_name }
+    surname { Faker::Name.last_name }
+    email { Faker::Internet.email }
+    password { '123456' }
+    password_confirmation { '123456' }
   end
 end
--- spec/requests/api/v1/teacher/sessions_spec.rb
+++ spec/requests/api/v1/teacher/sessions_spec.rb
@@ -2,9 +2,35 @@ require 'rails_helper'
 
 RSpec.describe "Api::V1::Teacher::Session", type: :request do
   describe "POST /api/v1/teacher/sessions" do
-    it "works! (now write some real specs)" do
-      get api_v1_teachers_path
-      expect(response).to have_http_status(200)
+    let!(:teacher) { create(:teacher) }
+
+    it 'should create a session for the teacher' do
+      req_params = {
+        email: teacher.email,
+        password: '123456'
+      }
+
+      post '/api/v1/teacher/sessions', params: req_params
+      payload = JSON.parse(response.body)
+
+      expect(payload['name']).to eql(teacher.name)
+      expect(response).to have_http_status(:ok)
     end
+
+    it 'should not create a session for the teacher' do
+      req_params = {
+        email: teacher.email,
+        password: '23'
+      }
+
+      post '/api/v1/teacher/sessions', params: req_params
+      payload = JSON.parse(response.body)
+
+      expect(payload['error']).to eql('Wrong email/password')
+      expect(payload['status']).to eql('not_found')
+
+
+    end
+
   end
 end
