7a7782a1653f1a89eef90f0c296c9590a9c69432
Heber Lazo <heberlazo@gmail.com>
2022-11-20 21:51:10 -0500

    Refactor seed

--- app/models/timetable.rb
+++ app/models/timetable.rb
@@ -1,4 +1,5 @@
 class Timetable < ApplicationRecord
+  belongs_to :subject
   enum :week_day, [ :Lunes, :Martes, :Miercoles, :Jueves, :Viernes, :Sabado, :Domingo ]
   validates_presence_of :starts_at, :ends_at, :week_day
 
--- db/seeds.rb
+++ db/seeds.rb
@@ -4,38 +4,51 @@ first_name = Faker::Name.first_name
 last_name = Faker::Name.middle_name
 password = '123456'
 Admin.create(name: first_name, surname: last_name, email: 'admin@gmail.com', password: password, password_confirmation: password)
-l= GradeLevel.create(grade_level: 1, grade: 5, section: 0)
+quinto_A= GradeLevel.create(grade_level: 1, grade: 5, section: 0)
+quinto_B= GradeLevel.create(grade_level: 1, grade: 5, section: 1)
+
+c5a = Classroom.create(grade_level_id: quinto_A.id, participants: 10)
+c5b = Classroom.create(grade_level_id: quinto_B.id, participants: 10)
 
 5.times do
   Course.create(name: Faker::Educator.subject)
 end
-c1 = Classroom.create(grade_level_id: l.id, participants: 10)
-2.times do |i|
+
+5.times do |i|
   first_name = Faker::Name.first_name
   last_name = Faker::Name.middle_name
   email = "teacher_#{i}@gmail.com"
   Teacher.create(name: first_name, surname: last_name, email: email, password: password, password_confirmation: password)
 end
 
-sub = Subject.create(times_week: '2', classroom_id: c1.id, course_id: Course.first.id, teacher_id: Teacher.first.id)
+classrooms = Classroom.all
+courses = Course.all
 
-sub.times_week.to_i.times do |i|
-  starts = [8,9,10].sample
-  Timetable.create(week_day: i, starts_at: starts, ends_at:starts + 2)
+classrooms.each_with_index do|classroom, index|
+  courses.each do |course|
+    times = [1,2].sample
+    teacher = Teacher.all.sample
+    sub = Subject.create(times_week: times, classroom_id: classroom.id, course_id: course.id, teacher_id: teacher.id)
+    sub.times_week.to_i.times do
+      day = [0,1,2,3,4].sample
+      starts = [8,9,10,11,12,13,14,15].sample
+      Timetable.create(week_day: day, starts_at: starts, ends_at:starts + 2, subject_id: sub.id)
+    end
+  end
+
+  classroom.participants.times {|i|
+    first_name = Faker::Name.first_name
+    last_name = Faker::Name.middle_name
+    email = "student_#{index}_#{i}@gmail.com"
+    Student.create(
+      name: first_name,
+      surname: last_name,
+      email: email,
+      password: password,
+      password_confirmation: password,
+      active: true,
+      classroom_id: classroom.id
+    )
+  }
 end
 
-
-10.times do |i|
-  first_name = Faker::Name.first_name
-  last_name = Faker::Name.middle_name
-  email = "student_#{i}@gmail.com"
-  Student.create(
-    name: first_name,
-    surname: last_name,
-    email: email,
-    password: password,
-    password_confirmation: password,
-    active: true,
-    classroom_id: c1.id
-  )
-end
