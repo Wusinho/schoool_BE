12254ed89a56f1414f6c8eb4a6100b42617d00f0
Heber Lazo <heberlazo@gmail.com>
2022-11-20 21:16:35 -0500

    Added assosiation and validation to subject

--- app/models/classroom.rb
+++ app/models/classroom.rb
@@ -1,7 +1,7 @@
 class Classroom < ApplicationRecord
   belongs_to :grade_level
   has_many :subjects
-  has_many :cursos, through: :subjects, source: :course
+  has_many :classroom_courses, through: :subjects, source: :course
   validates_presence_of :participants
   validates_numericality_of :participants, greater_than_or_equal_to: 1, less_than_or_equal_to: 50, message: 'El valor esta entre 1 y 50'
 end
--- app/models/subject.rb
+++ app/models/subject.rb
@@ -2,5 +2,8 @@ class Subject < ApplicationRecord
   has_many :timetables
   belongs_to :course
   belongs_to :classroom
+  belongs_to :teacher
   validates_presence_of :times_week
+  validates_numericality_of :times_week, greater_than_or_equal_to: 1, less_than_or_equal_to: 5, message: 'El valor esta entre 1 y 5'
+
 end
